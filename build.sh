da993c08953a9a2ec49e82b18aefe3a6ad236738
Senthamizhvelan M <senthamizhvelanm@gmail.com>
2026-08-06 14:26:45 +0530

    Fix Gunicorn timeout by pre-downloading FastEmbed model in build step

--- build.sh
+++ build.sh
@@ -10,3 +10,8 @@ cd frontend-react
 npm install
 npm run build
 cd ..
+
+# Pre-download the FastEmbed weights so Gunicorn doesn't timeout on boot
+echo "Pre-downloading FastEmbed model weights..."
+python -c "from langchain_community.embeddings.fastembed import FastEmbedEmbeddings; FastEmbedEmbeddings()"
+echo "Done!"
